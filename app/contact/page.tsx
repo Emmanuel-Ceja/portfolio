004b67abc685fbce31314a4b174c8b5b193531dd
Emmanuel <Emmanuelceja135@outlook.com>
2025-12-31 00:49:38 -0800

    added contact and portfolio page as well as fixed the homepage scaling

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <div className= "text-[#787878]">
+            <p>DIS THE CONTACT PAGE</p>
+        </div>
+
+    )
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,7 @@
+export default function Portfolio() {
+    return (
+        <div className= "text-[#787878]"> 
+            <p>DIS THE PORTFOLIO PAGE</p>
+        </div>
+    )
+}
